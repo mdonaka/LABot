6b4da0bedd1aadbf160211e2bfb73c6708462920
Nakata <4ryo2-meron-math@ezweb.ne.jp>
2019-11-05 15:26:05 +0900

    add requests to slack

--- bot.py
+++ bot.py
@@ -1,1 +1,9 @@
-print("hello world")
+import json
+import requests
+
+with open("data.json", "r") as f:
+    data = json.load(f)
+    URL = data["WebhookURL"]
+    requests.post(URL, data=json.dumps({
+        'text': u'Hello World From Python.'
+    }))
