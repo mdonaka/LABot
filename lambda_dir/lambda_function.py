c5c9729f54a259881087059f26edb4b29354e462
Nakata <4ryo2-meron-math@ezweb.ne.jp>
2019-11-06 17:34:40 +0900

    fix add calender.py again

--- lambda_dir/lambda_function.py
+++ lambda_dir/lambda_function.py
@@ -2,21 +2,22 @@ import os
 import json
 import requests
 from datetime import date
-from calender import GoogleCalendar
-from decode import decode
+from .calender import GoogleCalendar
+# from decode import decode
 
 def lambda_handler(event, context):
     URL = os.environ["WebhookURL"]
     ID = os.environ['GOOGLE_SERVICE_ACCOUNT_ID']
-    URL = decode(URL)
-    ID = decode(ID)
+    #URL = decode(URL)
+    #ID = decode(ID)
 
     calender = GoogleCalendar(ID)
     frm = date(year=2019, month=11, day=1).isoformat() + "T00:00:00.000000Z"
     to = date(year=2019, month=11, day=29).isoformat() + "T00:00:00.000000Z"
     schedule = calender.get_schedule(frm,to)
 
-    text = schedule[0]
+    text =str(schedule)
+
     requests.post(URL, data=json.dumps({
         'text': text
     }))
