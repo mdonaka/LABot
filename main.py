eb6e539ff0aeacae8d684050167e41f17bd11520
Nakata <4ryo2-meron-math@ezweb.ne.jp>
2019-11-06 17:07:02 +0900

    add using google calender

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 """For local"""
 import os
+import sys
 from os.path import join, dirname
 from dotenv import load_dotenv
+os.chdir("./lambda_dir/")
 
 from lambda_dir.lambda_function import lambda_handler
 
